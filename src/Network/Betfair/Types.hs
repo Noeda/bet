da13d41bef1a83351f8a3dcfbb1f6710e19501eb
Mikko Juola <mikjuo@gmail.com>
2014-11-17 07:59:51 +0200

    More betfair types.

--- src/Network/Betfair/Types.hs
+++ src/Network/Betfair/Types.hs
@@ -2,10 +2,14 @@
 {-# LANGUAGE AutoDeriveTypeable #-}
 {-# LANGUAGE TemplateHaskell #-}
 
--- | This module defines most data types in the betting API of Betfair API.
+-- | This module defines most data types in the betting API of Betfair API,
+-- corresponding to version 2.0.
 --
 -- Where possible, data types are in 1:1 correspondence to the ones documented
--- in the API.
+-- in the API. This is not always possible because there are name clashes. So
+-- if you see spurious letters after some names then that's name clashes being
+-- resolved the ugly way. (Maybe in the future we'll use
+-- OverloadedRecordFields?).
 --
 -- All types have lenses where they can.
 --
@@ -16,16 +20,19 @@ module Network.Betfair.Types
     , MarketFilter(..)
     , HasMarketFilter(..)
     -- * IDs
-    , EventTypeId(..)
-    , EventId(..)
     , Country(..)
-    , MarketTypeCode(..)
     , CompetitionId(..)
+    , EventId(..)
+    , EventTypeId(..)
     , MarketId(..)
+    , MarketTypeCode(..)
+    , SelectionId(..)
     , Venue(..)
     -- * Enums
+    , MarketBettingType(..)
+    , MarketProjection(..)
     , MarketSort(..)
-    , MarketProjection(..) )
+    , OrderStatus(..) )
     where
 
 import Control.Lens
@@ -34,7 +41,8 @@ import Data.Text ( Text )
 import Data.Time
 import Data.Typeable
 import qualified Data.Set as S
-import Prelude hiding ( filter )
+import qualified Data.Map.Lazy as M
+import Prelude hiding ( filter, id )
 
 newtype EventTypeId = EventTypeId { getEventTypeId :: Text }
                       deriving ( Eq, Ord, Show, Read, Typeable
@@ -59,6 +67,10 @@ newtype CompetitionId = CompetitionId { getCompetitionId :: Text }
 newtype MarketId = MarketId { getMarketId :: Text }
                    deriving ( Eq, Ord, Show, Read, Typeable
                             , FromJSON, ToJSON )
+
+newtype SelectionId = SelectionId { getSelectionId :: Text }
+                      deriving ( Eq, Ord, Show, Read, Typeable
+                               , FromJSON, ToJSON )
 
 newtype Venue = Venue { getVenue :: Text }
                 deriving ( Eq, Ord, Show, Read, Typeable
@@ -86,7 +98,7 @@ data MarketProjection
     | Event
     | EventType
     | MarketStartTime
-    | MarketDescription
+    | MarketDescriptionE
     | RunnerDescription
     | RunnerMetadata
     deriving ( Eq, Ord, Show, Read, Typeable, Enum )
@@ -122,6 +134,67 @@ data MarketFilter = MarketFilter
     , _withOrders :: Maybe (S.Set OrderStatus) }
     deriving ( Eq, Ord, Show, Read, Typeable )
 
+data MarketCatalogue = MarketCatalogue
+    { _marketId :: MarketId
+    , _marketName :: Text
+    , _mcmarketStartTime :: Maybe UTCTime
+    , _description :: Maybe MarketDescription
+    , _totalMatched :: Maybe Double
+    , _runners :: Maybe [RunnerCatalog]
+    , _eventType :: Maybe EventTypeC
+    , _competition :: Maybe Competition
+    , _event :: Maybe Event }
+    deriving ( Eq, Ord, Show, Read, Typeable )
+
+data MarketDescription = MarketDescription
+    { _persistenceEnabled :: Bool
+    , _bspMarket :: Bool
+    , _marketTime :: UTCTime
+    , _suspendTime :: UTCTime
+    , _settleTime :: Maybe UTCTime
+    , _bettingType :: MarketBettingType
+    , _mdturnInPlayEnabled :: Bool
+    , _marketType :: Text
+    , _regulator :: Text
+    , _marketBaseRate :: Double
+    , _discountAllowed :: Bool
+    , _wallet :: Maybe Text
+    , _rules :: Maybe Text
+    , _rulesHasDate :: Maybe Bool
+    , _clarifications :: Maybe Text }
+    deriving ( Eq, Ord, Show, Read, Typeable )
+
+data EventTypeC = EventTypeC
+    { _id :: EventTypeId
+    , _name :: Text }
+    deriving ( Eq, Ord, Show, Read, Typeable )
+
+data Competition = CompetitionC
+    { _cid :: CompetitionId
+    , _cname :: Text }
+    deriving ( Eq, Ord, Show, Read, Typeable )
+
+data Event = EventC
+    { _eid :: EventId
+    , _ename :: Text
+    , _countryCode :: Country
+    , _timezone :: Text
+    , _venue :: Text
+    , _openDate :: UTCTime }
+    deriving ( Eq, Ord, Show, Read, Typeable )
+
+data RunnerCatalog = RunnerCatalog
+    { _selectionId :: SelectionId
+    , _runnerName :: Text
+    , _handicap :: Double
+    , _sortPriority :: Int
+    , _metadata :: M.Map Text Text }
+    deriving ( Eq, Ord, Show, Read, Typeable )
+
 makeClassy ''ListMarketCatalogue
 makeClassy ''MarketFilter
+makeClassy ''MarketCatalogue
+makeClassy ''MarketDescription
+makeClassy ''RunnerCatalog
+makeClassy ''EventTypeC
 
